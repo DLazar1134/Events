d4288d3426e0f0b09e2ea9f5ea35ce47e4df81a9
wasadmin <rabaraaaron@gmail.com>
2023-08-09 19:02:01 +0000

    Changed database. Added annotations.

--- src/main/java/com/mcnz/project/ServiceGateway.java
+++ src/main/java/com/mcnz/project/ServiceGateway.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
+@Transactional
 public class ServiceGateway {
 	
 @Autowired
